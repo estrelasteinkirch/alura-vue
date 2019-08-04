85575cc883c948d714e9f050b4bacccc7be991e7
Estrela <estrela.steinkirch@gmail.com>
2019-08-04 20:24:35 +0200

    Configura as rotas

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,15 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-      <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
-      <ul class="lista-fotos">
-
-        <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto._id">
-
-          <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-          </meu-painel>
-       
-        </li>
-
-    </ul>
+    <!-- é aqui que encontrará os componentes de página que queremos visualizar -->
     
+      <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva.vue';
-
 export default {
 
-  components: {
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
-
-  data() {
-    return {
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: ''
-    }
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        // filtra a lista
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        // se o campo estiver vazio, não filtramos, retornamos a lista completa
-        return this.fotos;
-      }
-    }
-  },
-
-  created(){
-    let promise = this.$http.get('http://localhost:3000/v1/fotos');
-
-    promise
-      .then(res => res.json()) 
-      // res.json também é uma promise
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
 }
 </script>
 
@@ -67,23 +19,6 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
-  }
-
-  .centralizado {
-    text-align: center;
-  }
-
-  .lista-fotos{
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+// importando o router!
+import VueRouter from 'vue-router';
+// tem que vir entre chaves, porque não é default
+import { routes } from './routes';
 
-Vue.use(VueResource);
+const router = new VueRouter({
+  routes : routes,
+  mode: 'history'
+});
+
+// registrando o router
+Vue.use(VueRouter);
+
+Vue.use(VueResource); 
 
 new Vue({
   el: '#app',
+  router,
+  
   render: h => h(App)
 })
