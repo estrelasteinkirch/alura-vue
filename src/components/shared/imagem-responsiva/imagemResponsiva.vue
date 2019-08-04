a66e4f63b97f06645f638e2de9eb196884699a0c
Estrela <estrela.steinkirch@gmail.com>
2019-08-04 20:25:21 +0200

    Atualiva os componentes e as rotas

---
+++ src/components/shared/imagem-responsiva/imagemResponsiva.vue
@@ -0,0 +1,18 @@
+<template>
+    <img class="imagem-responsiva" v-bind:src="url" :alt="titulo">
+      <!-- dentro do atributo, nao é possível usar a interpolação, entao tem que usar o v-bind. Um atalho para v-bind é usar apenas o : (como em :alt) -->  
+</template>
+
+<script>
+
+export default {
+    props: ['url', 'titulo']
+}
+
+</script>
+
+<style scoped>
+  .imagem-responsiva {
+      width: 100%;
+  }
+</style>
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/home/Home.vue';
+import Cadastro from './components/cadastro/Cadastro.vue';
+
+
+
+export const routes = [
+
+  { path: '', component: Home },
+  { path: '/cadastro', component: Cadastro }
+
+];
