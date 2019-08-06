9c0084cd8b84074911f46d4c3aa83722578d1b49
Estrela <estrela.steinkirch@gmail.com>
2019-08-06 17:32:51 +0200

    Faz o deploy localmente
    Realiza code spliting e lazy loading

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import Home from './components/home/Home.vue';
-import Cadastro from './components/cadastro/Cadastro.vue';
+// import Cadastro from './components/cadastro/Cadastro.vue';
+//Lazy loading:
+// const Cadastro = () =>MediaKeySystemAccess.import('./components/cadastro/Cadastro.vue'); ->>> esse nao funcionou
+const Cadastro = resolve => require(['./components/cadastro/Cadastro.vue'], m => resolve(m.default));
 
 
 
